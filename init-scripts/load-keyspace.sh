838611bebff486de4a892988b91f36316d893f1b
Gabriel Neme <gaboneme11@gmail.com>
2023-06-25 17:21:51 -0400

    First commit

---
+++ init-scripts/load-keyspace.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Waiting for Cassandra to start..."
+sleep 30
+
+echo "Loading Cassandra keyspace..."
+cqlsh node1 -f /docker-entrypoint-initdb.d/01-keyspace.cql
+echo "Keyspace and table create"
